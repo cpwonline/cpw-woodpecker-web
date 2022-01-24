a19e3e525a0bdb90a298b51b4d90a29b9e289063
Jose F Rivas C <josefelixrc7@gmail.com>
2022-01-24 18:01:44 +0000

    feat: Add REST calls to users

--- assets/js/api_rest_calls.js
+++ assets/js/api_rest_calls.js
@@ -6,6 +6,7 @@ $(function(){
 		// Click on menus
 			$('#cpw-woodpecker #sidebar_section #main_menu a[href="#business"]').click(function(e){
 				GetBusinessTable();
+				GetUsersTable();
 			});
 
 });
@@ -39,6 +40,35 @@ const GetBusinessTable = () => {
 
 			let row = $("<tr></tr>").append(cell1, cell2, cell3, cell4, options);
 			$("#cpw-woodpecker #business_table tbody").append(row);
+		}
+	});
+}
+
+const GetUsersTable = () => {
+	let data = {"pair-information": [{"auth": {"user": "root", "password": "root_password"}}, {"data": [{"type": "fields", "contents": ["id", "username", "image", "reg_date"]}]}]};
+	let call = new APIRESTSystem("users", "GET", data);
+
+	call.MakeHTTPRequest().then(data => {
+		$("#cpw-woodpecker #users_table tbody").empty();
+
+		for(let key in data.results){
+			let cell1 = $("<th scope='row'></th>").text(data.results[key].id);
+			let cell2 = $("<td></td>").text(data.results[key].username);
+
+			let cell3;
+			if(data.results[key].image === undefined)
+				cell3 = $("<td></td>").append("<img width='40' src='../../assets/images/blank_avatar.svg'/>");
+			else{
+				let image = $('<img alt="' + data.results[key].username + '" />');
+				let blob = new Blob([data.results[key].image], {type: 'image/jpeg'});
+				$(image).attr("src", window.URL.createObjectURL(blob));
+				cell3 = $("<td></td>").append(image);
+			}
+
+			let cell4 = $("<td></td>").text(data.results[key].reg_date);
+
+			let row = $("<tr></tr>").append(cell1, cell2, cell3, cell4, options);
+			$("#cpw-woodpecker #users_table tbody").append(row);
 		}
 	});
 }
